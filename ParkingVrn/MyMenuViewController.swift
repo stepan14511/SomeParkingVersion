9a9bc5670e977d8b6984112f6eb18b5e18ca3b94
Stepan Kuznetsov <stepan14511@gmail.com>
2020-10-19 21:58:25 +0300

    Save point

---
+++ ParkingVrn/MyMenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MenuViewController.swift
+//  ParkingVrn
+//
+//  Created by Stepan Kuznetsov on 19.10.2020.
+//
+
+import UIKit
+import SideMenu
+
+class MyMenuViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ ParkingVrn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParkingVrn
+//
+//  Created by Stepan Kuznetsov on 18.10.2020.
+//
+
+import UIKit
+import SideMenu
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
